224c58d141d9fc47e1a13e8f782857f753b01bac
PradeeppaPalanisamy <pradeeppapk05@gmail.com>
2024-12-29 22:06:49 +0530

    Web page update

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import Form from './Form.jsx'
+import Apple from './Apple.jsx'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Hello World! 😊</h1>
+      <Form />
+      <Apple />
+    </>
+  ) 
+}
+
+export default App
+
+
---
+++ src/Slam.jsx
@@ -0,0 +1,35 @@
+import './Slam.css'
+function Slam(){
+    return(
+        <div>
+            
+            <div>
+                <form class="form">
+                    <label>Name:</label>
+                    <input class="nm" type="text" placeholder="Enter your name"></input>
+                    <label for ="phone">Number:</label>
+                    <input class="name" type="text" placeholder="Enter your number"></input>
+                    <label for="address">Address:</label>
+                    <textarea class="add" placeholder="Enter your address"></textarea>
+                    <label>Friend In:
+                    <label><input class="frnd" type="radio" name="friend"></input>School</label>
+                    
+                    <label><input class="frnd" type="radio" name="friend"></input>College</label></label>
+                    <label>DOB:
+                    <input class ="date" type="date"></input></label>
+                    <label>Favourite Actors:</label>
+                    <label><input class="act" type="checkbox"></input>Ajith</label>
+                    <label><input class="act" type="checkbox"></input>Surya</label>
+                    <label><input class="act" type="checkbox"></input>Karthi</label>
+                    <label><input class="act" type="checkbox"></input>DQ</label>
+                    <label><input class="actor" type="checkbox"></input>Ram Charan</label>
+                    <label for="memories">Share our best memories:</label>
+                    <textarea class="add" placeholder="Share our memories"></textarea>
+                    <input type="submit" class="submit"></input>
+                </form>
+            </div>
+
+        </div>
+    )
+}
+export default Slam
